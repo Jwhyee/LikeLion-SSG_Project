f7f55d4e27d62a10feecfe3445ce6ffaa193af20
Jwhyee <kawnsdud@naver.com>
2022-07-07 14:43:52 +0900

    App, Main 클래스 생성

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App{
+    public void run() throws IOException {
+        System.out.println("== 명언 SSG ==");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        outer: while (true) {
+            System.out.print("명령 ) ");
+            String cmd = br.readLine();
+
+            switch (cmd) {
+                case "종료":
+                    break outer;
+            }
+        }
+        br.close();
+    }
+}
