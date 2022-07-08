f308f464ef5636c5bd18fcf600dba083ee4c6a1a
Jwhyee <kawnsdud@naver.com>
2022-07-08 14:15:39 +0900

    미리 입력된 명령를 받아들일 수 있는 스캐너 객체

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,28 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
-    public void addTest1() {
+    public void scannerTest() {
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
 
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
     }
 }
