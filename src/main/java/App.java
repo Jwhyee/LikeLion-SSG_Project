817281cb5476148696d4042277ae8de948a27354
Jwhyee <kawnsdud@naver.com>
2022-07-08 14:44:26 +0900

    입력 받은 명언을 리스트에 저장 + 목록 기능 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App{
     public void run() throws IOException {
@@ -6,6 +8,7 @@ public class App{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        List<WiseSaying> wiseSayings = new ArrayList<>();
         int wiseSayingLastId = 0;
 
         outer: while (true) {
@@ -22,10 +25,16 @@ public class App{
                     String author = br.readLine();
                     int id = ++wiseSayingLastId;
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.println(id + "번 명언이 등록되었습니다.");
                     break;
                 case "목록":
                     System.out.println("번호 / 작가 / 명언");
+                    System.out.println("---------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying1 = wiseSayings.get(i);
+                        System.out.println(wiseSaying1.id + " / " + wiseSaying1.author + " / " + wiseSaying1.content);
+                    }
                     break;
                 case "삭제":
                     break;
