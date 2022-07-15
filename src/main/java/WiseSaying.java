f29a01b1df113eccf84297aa62f958a44a419ecc
Jwhyee <kawnsdud@naver.com>
2022-07-15 13:31:03 +0900

    2, 3단계 명언 등록, 번호 노출 구현

---
+++ src/main/java/WiseSaying.java
@@ -0,0 +1,11 @@
+public class WiseSaying {
+    private int id;
+    private String author;
+    private String content;
+
+    public WiseSaying(int id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+}
