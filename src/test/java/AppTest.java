74cf776a74dd37324004ce6bbca1f76e6da566ee
Jwhyee <kawnsdud@naver.com>
2022-07-15 13:31:22 +0900

    2, 3단계 명언 등록, 번호 노출 테스트

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void 등록() {
+        String cmd = "등록";
+        String ws = "현재를 사랑하라";
+        String aut = "작자미상";
+
+        assertEquals(cmd, "등록");
+        assertEquals(ws, "현재를 사랑하라");
+        assertEquals(aut, "작자미상");
+
+    }
+
+    @Test
+    void 등록시_생성된_명언번호_노출() {
+        Map<Integer, WiseSaying> map = new HashMap<>();
+        int id = 1;
+        String cmd = "등록";
+        String content = "현재를 사랑하라";
+        String author = "작자미상";
+        WiseSaying saying = new WiseSaying(id, author, content);
+
+        assertEquals(id, 1);
+    }
+
+}
