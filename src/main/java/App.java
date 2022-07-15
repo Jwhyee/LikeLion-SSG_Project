feba8d90a993b807fe8f0658df54e17ad92388d7
Jwhyee <kawnsdud@naver.com>
2022-07-15 14:38:19 +0900

    App, WiseSaying 리팩터링

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,85 +1,96 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
-    List<WiseSaying> wsRepo;
     private Scanner sc;
-    WiseSaying wiseSaying;
-    Rq rq;
+    private List<WiseSaying> wiseSayings;
+    private int wiseSayingLastId;
 
     public App() {
         sc = new Scanner(System.in);
+        wiseSayings = new ArrayList<>();
+        wiseSayingLastId = 0;
     }
 
     public void run() {
-        wsRepo = new ArrayList<>();
         System.out.println("== 명언 SSG ==");
-        int wiseSayingLastId = 0;
 
         outer:
         while (true) {
-            System.out.print("명령) ");
-            String cmd = sc.nextLine();
-            rq = new Rq(cmd);
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            Rq rq = new Rq(cmd);
+
             switch (rq.getPath()) {
                 case "등록":
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-                    int id = ++wiseSayingLastId;
-                    wiseSaying = new WiseSaying(id, author, content);
-                    wsRepo.add(wiseSaying);
-                    System.out.println(id + "번 명언이 등록되었습니다.");
-                    id++;
-                    break;
-                case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("-------------------");
-                    for (WiseSaying saying : wsRepo) {
-                        System.out.println(saying.getId() + " / " + saying.getAuthor() + " / " + saying.getContent());
-                    }
+                    write(rq);
                     break;
                 case "삭제":
-                    int paramId = rq.getIntParam("id", 0);
-                    if (paramId == 0) {
-                        System.out.println("id를 입력해주세요.");
-                        continue;
-                    }
-                    WiseSaying findWiseSaying = null;
-                    for (WiseSaying saying : wsRepo) {
-                        if (saying.getId() == paramId) {
-                            findWiseSaying = saying;
-                        }
-                    }
-                    if (findWiseSaying == null) {
-                        System.out.println(paramId + "번 명언은 존재하지 않습니다.");
-                        continue;
-                    }
-                    wsRepo.remove(findWiseSaying);
-                    System.out.println(paramId + "번 명언이 삭제되었습니다.");
+                    remove(rq);
                     break;
-                case "수정":
-                    int findParamId = rq.getIntParam("id", 0);
-                    if (findParamId == 0) {
-                        System.out.println("id를 입력해주세요.");
-                        continue;
-                    }
-                    WiseSaying findWise = null;
-                    for (WiseSaying saying : wsRepo) {
-                        if (saying.getId() == findParamId) {
-                            findWise = saying;
-                        }
-                    }
-                    if (findWise == null) {
-                        System.out.println(findParamId + "번 명언은 존재하지 않습니다.");
-                        continue;
-                    }
-                    wsRepo.remove(findWise);
-                    System.out.println(findParamId + "번 명언이 삭제되었습니다.");
+                case "목록":
+                    list(rq);
                     break;
                 case "종료":
                     break outer;
             }
         }
+
+        sc.close();
+    }
+
+    private void list(Rq rq) {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("-------------------");
+        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+            WiseSaying wiseSaying_ = wiseSayings.get(i);
+            System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+        }
+    }
+
+    private void write(Rq rq) {
+        System.out.printf("명언 : ");
+        String content = sc.nextLine().trim();
+        System.out.printf("작가 : ");
+        String author = sc.nextLine().trim();
+        int id = ++wiseSayingLastId; // 명언 글 번호 증가
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+    }
+
+    private void remove(Rq rq) {
+        // URL에 입력된 id 얻기
+        int paramId = rq.getIntParam("id", 0);
+
+        // URL에 입력된 id가 없다면 작업중지
+        if (paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+
+        // URL에 입력된 id에 해당하는 명언객체 찾기
+        WiseSaying foundWiseSaying = null;
+
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.id == paramId) {
+                foundWiseSaying = wiseSaying;
+            }
+        }
+
+        // 찾지 못했다면 중지
+        if (foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다..\n", paramId);
+            return;
+        }
+
+        // 입력된 id에 해당하는 명언객체를 리스트에서 삭제
+        wiseSayings.remove(foundWiseSaying);
+
+        System.out.printf("%d번 명언이 삭제되었습니다.\n", paramId);
     }
 }
--- src/main/java/WiseSaying.java
+++ src/main/java/WiseSaying.java
@@ -1,24 +1,12 @@
 public class WiseSaying {
-    private int id;
-    private String author;
-    private String content;
+    int id;
+    String author;
+    String content;
 
     public WiseSaying(int id, String author, String content) {
         this.id = id;
         this.author = author;
         this.content = content;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getContent() {
-        return content;
     }
 
     @Override
