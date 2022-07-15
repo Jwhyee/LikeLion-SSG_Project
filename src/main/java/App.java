74928e9cc95440f84144586d86c2fd173c664012
Jwhyee <kawnsdud@naver.com>
2022-07-15 13:16:15 +0900

    1단계 종료 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,5 +10,13 @@ public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
 
+        outer:
+        while (true) {
+            String cmd = sc.nextLine();
+            switch (cmd) {
+                case "종료":
+                    break outer;
+            }
+        }
     }
 }
