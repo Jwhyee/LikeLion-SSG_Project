9f0caa1d7865803f63136f5b181e6367580fe49e
Jwhyee <kawnsdud@naver.com>
2022-07-15 15:05:15 +0900

    Util파일 추가

---
+++ src/main/java/util/Util.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.io.*;
+import java.util.Scanner;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Util {
+    public static void saveToFile(String path, String body) {
+        try (RandomAccessFile stream = new RandomAccessFile(path, "rw");
+             FileChannel channel = stream.getChannel()) {
+            byte[] strBytes = body.getBytes();
+            ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
+            buffer.put(strBytes);
+            buffer.flip();
+            channel.write(buffer);
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void mkdir(String path) {
+        File dir = new File(path);
+        dir.mkdirs();
+    }
+
+    public static String readFromFile(String path) {
+        try (RandomAccessFile reader = new RandomAccessFile(path, "r")) {
+            String line = reader.readLine();
+            return new String(line.getBytes("iso-8859-1"), "utf-8");
+        } catch (IOException e) {
+        }
+
+        return "";
+    }
+}
