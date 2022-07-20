01706d45a4cc31cb5d7a4a37c7d9119bb2388a13
Jwhyee <kawnsdud@naver.com>
2022-07-20 17:27:51 +0900

    Project 초기화

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private Scanner sc;
 
     public App() {
-        sc = new Scanner(System.in);
+
     }
 
     public void run() {
-        System.out.println("== 명언 SSG ==");
-
-        WiseSayingController wiseSayingController = new WiseSayingController(sc);
-
-        outer:
-        while (true) {
-            System.out.printf("명령) ");
-            String cmd = sc.nextLine().trim();
-
-            Rq rq = new Rq(cmd);
-
-            switch (rq.getPath()) {
-                case "등록":
-                    wiseSayingController.write(rq);
-                    break;
-                case "삭제":
-                    wiseSayingController.remove(rq);
-                    break;
-                case "수정":
-                    wiseSayingController.modify(rq);
-                    break;
-                case "목록":
-                    wiseSayingController.list(rq);
-                    break;
-                case "종료":
-                    break outer;
-            }
-        }
-
-        sc.close();
+        String str1 = "안녕";
+        String str2 = "안녕";
+        System.out.println(str1 == str2); // true
+        str1 += "1";
+        str2 = "안녕1";
+        System.out.println(str1 == str2); // false
+        System.out.println(str1.equals(str2)); // true
     }
 }
